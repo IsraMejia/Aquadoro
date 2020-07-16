9c668a85c98aba7ab7cf27a41292556c0f71fbf6
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-07-15 23:53:43 -0500

    Se corrigieron errores en pruebas, ahora se pasara goalCard

--- lib/pantalla_prueba.dart
+++ lib/pantalla_prueba.dart
@@ -11,64 +11,133 @@ class PantallaPrueba extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pruebas'),
       ),
-      body:  Container(
-        margin: EdgeInsets.all(10.0),
+      body:  Column(
+        children: <Widget>[
+          Container(
+        margin: EdgeInsets.all(25.0),
         child: Row(
           
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start ,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween ,
         children: <Widget>[
-          Container( //entrada de la actividad
-            width: anchoPantalla*0.45,
-            padding: EdgeInsets.symmetric( horizontal: 5.0),
+          Expanded( //entrada de la actividad
+           
             child: _actividadInput(),
           ),
 
           Container(
-            width: anchoPantalla*0.2,
+            
+            width: anchoPantalla*0.15,
             child: _inputConcentracion()),
           Container(
-            width: anchoPantalla*0.2,
+            // padding: EdgeInsets.only(left:0.02),
+            width: anchoPantalla*0.15,
             child: _inputDescanso()),
               
-            
-          
           
           Container(//boton para iniciar el pomodoro
-            width: anchoPantalla*0.05,
+            width: anchoPantalla*0.1,
+            alignment: Alignment.center ,
             child: FlatButton(
-              
               onPressed: (){},
-               child: Icon(Icons.chevron_right),
+               child: Icon(Icons.keyboard_arrow_right , size :anchoPantalla*0.1, ),
             ),
           ),
 
          ],
         ),
       ),
+    
+//---------------------------------------------------
+        Container(
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[50],
+          borderRadius: BorderRadius.circular(10.0) ,
+        ),  
+        margin: EdgeInsets.all(20.0),
+        child: Row(
+          
+        crossAxisAlignment: CrossAxisAlignment.start ,
+        children: <Widget>[
+          Container( //entrada de la actividad
+            padding: EdgeInsets.only(left: 10.0),
+            width: anchoPantalla*0.45,
+            child: _actividadInput(),
+          ), 
+          Spacer(),
+          Container( 
+            width: anchoPantalla*0.15,
+            child: _inputConcentracion()),
+          Spacer(),
+          Container( 
+            width: anchoPantalla*0.15,
+            child: _inputDescanso()),
+          
+          Container(//boton para iniciar el pomodoro
+            width: anchoPantalla*0.135,
+            // padding: EdgeInsets.only(right: 30.0),
+            child: Align(
+              alignment: Alignment.topLeft ,
+              child: FlatButton(
+               onPressed: (){},
+               child: Icon(
+                Icons.timer ,
+                size :anchoPantalla*0.1, 
+                color: Colors.cyan[700],
+               ),
+              ) ,
+            )
+            
+          ),
+// Spacer()
+         ],
+        ),
+      )
+    
+
+
+        ],
+      )
+
+
+      
+    
+
+    
+    
+    
     );
   }
 
     Widget _actividadInput(){
      return TextFormField(
-      decoration: InputDecoration(
+      decoration: InputDecoration( 
+      // hintText: 'Actividad',
+      // hintStyle: TextStyle(fontSize: 14.0),
+      labelText: 'Actividad',
+      labelStyle: TextStyle(fontSize: 13.0 ),
       
-      hintText: 'Actividad',
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ), 
       ),
-      // onSaved : (actividad)=> widget.actividad = actividad
+      onSaved : (actividad){
+        print(actividad);
+      } 
+      // onChanged: (act){
+      //   print(act);
+      // },
+      // onEditingComplete: , //Al parecer usa lo del focus
       
     );
   }//_actividadInput()
 
   Widget _inputDescanso(){
     return TextFormField(
-      //  initialValue:  'Descanso',
+      
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
-        labelText: "minutos",
-        hintText: 'Descanso',
+        labelText: 'Descanso',
+        labelStyle: TextStyle(fontSize: 13.0 ),
+        hintText: 'minutos' ,
+        hintStyle: TextStyle(fontSize: 10.0),
       ),
       // onSaved : (tiempoDescanso)=> widget.tDescanso  = double.parse(tiempoDescanso) as int ,
     );
@@ -76,11 +145,13 @@ class PantallaPrueba extends StatelessWidget {
 
   Widget _inputConcentracion(){
     return TextFormField(
-      //  initialValue:  'Concentracion',
+      
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
-        labelText: "minutos",
-        hintText: 'Concentracion',
+        labelText: 'Enfoque',
+        hintText: 'minutos' ,
+        hintStyle: TextStyle(fontSize: 10.0),
+        labelStyle: TextStyle(fontSize: 13.0 ),
       ),
       // onSaved : (tiempoConcentracion)=> widget.tConcentracion  = double.parse(tiempoConcentracion) as int ,
     );

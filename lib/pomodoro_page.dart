f588fad7ce1a2ac43289d60c4a1e48be03ec19e1
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-07-29 16:46:00 -0500

    Si tenia que ser stateless jaja

--- lib/pomodoro_page.dart
+++ lib/pomodoro_page.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class PomodoroPage extends StatefulWidget {
+class PomodoroPage extends StatefulWidget
+ {
   // PomodoroPage({
   //   this.actividad,
   // });

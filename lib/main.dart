78396e923b1152f51a233500133070d5788e663f
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-08-23 17:12:44 -0500

    Se borro la pantalla pomodoro que ya no se usaba

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:aquadoro/goals_page.dart';
-import 'package:aquadoro/pomodoro_page.dart';
+import 'package:aquadoro/goals_page.dart'; 
 import 'package:aquadoro/pantalla_carga.dart';
 //import 'package:aquadoro/aquadoro.dart';
  
@@ -17,8 +16,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blue[800],
       ),
       routes: {      
-        'goalsPage'       : (BuildContext context) => GoalsPage(),
-        'pomodoroPage'    : (BuildContext context) => PomodoroPage(),   
+        'goalsPage'       : (BuildContext context) => GoalsPage(), 
         'pantallaDeCarga' : (BuildContext context) => PantallaDeCarga(), 
         //'aquadoro'        : (BuildContext context) => Aquadoro(), 
         //Ya no es necesario ya que se muestra la otra manera de mandar la ruta , 
